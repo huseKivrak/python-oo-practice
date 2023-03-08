6493d589ec4249b38d81f2b4e3154fbc7121ec0c
Vaughn Seekamp <vseekamp@gmail.com>
2023-03-07 16:40:57 -0800

    got first part of random working

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,23 @@
+from random import choice
+
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+    def __init__(self, path):
+        self.path = path
+        self.file = self.open_file()
+        self.words = self.words_read()
+        self.words_length = self.len_of_words_read()
+        print( f"{self.words_length} words read")
+
+    def open_file(self):
+        return open(f"{self.path}", "r")
+
+
+    def words_read(self):
+        return [word for word in self.file]
+
+    def len_of_words_read(self):
+        return len(self.words)
+
+    def random(self):
+        return choice(self.words)[2:-1]
