508aaae330ee69110e4b26d9357fe66f340ffaf0
Huse Kivrak <huse7@pm.me>
2023-03-07 20:41:09 -0500

    added doclines

--- wordfinder.py
+++ wordfinder.py
@@ -10,27 +10,34 @@ class WordFinder:
         print( f"{self.words_length} words read")
 
     def open_file(self):
+        """Opens file at path"""
         return open(f"{self.path}", "r")
 
 
     def words_read(self):
+        """adds read lines to words"""
         return [word[:-1] for word in self.file]
 
     def len_of_words_read(self):
+        """Returns length of word list"""
         return len(self.words)
 
     def random(self):
+        """Returns random words from words"""
         return choice(self.words)
 
 
 class SpecialWordFinder(WordFinder):
+    """defines SpecialWordFinder instances, extends WordFinder"""
 
     def __init__(self, path):
+        """constructs SpecialWordFinderInstances, adding real_words property"""
         super().__init__(path)
         self.real_words = self.get_words()
 
 
     def get_words(self):
+        """Checks for and returns words that are not comments or blank lines"""
         reals = []
         for line in self.words:
             if not line.startswith('#') and line != "":
